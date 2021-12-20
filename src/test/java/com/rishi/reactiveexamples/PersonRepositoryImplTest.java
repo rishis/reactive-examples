9feffe1b510fab23374dded3f854d21798eaa159
Rishi Shehrawat <rishishehrawat@gmail.com>
2021-12-20 13:12:37 +0530

    Added Flux test

--- src/test/java/com/rishi/reactiveexamples/PersonRepositoryImplTest.java
+++ src/test/java/com/rishi/reactiveexamples/PersonRepositoryImplTest.java
@@ -4,7 +4,10 @@ import com.rishi.reactiveexamples.domain.Person;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,5 +39,33 @@ class PersonRepositoryImplTest {
            System.out.println("op in sub"+person.toString());
         });
         System.out.println("##**");
+    }
+
+    @Test
+    void testFluxBlock() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Person person = personFlux.blockFirst();
+        System.out.println("Flux person "+person);
+    }
+
+    @Test
+    void testFluxSubscribe() {
+        Flux<Person> personFlux = personRepository.findAll();
+        personFlux.subscribe(person -> {
+            System.out.println("Flux subscribe"+person);
+        });
+        System.out.println("After subscribe method flux");
+
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Mono<List<Person>> personListMono = personFlux.collectList();
+        personListMono.subscribe(list -> {
+            list.forEach(person -> {
+                System.out.println("testFluxList**"+person);
+            });
+        });
     }
 }
